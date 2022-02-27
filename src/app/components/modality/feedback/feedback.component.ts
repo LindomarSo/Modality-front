90b89dc32bf5285a97a751088c64b398d045788f
Lindomar <lindomardias21@gmail.com>
2022-02-27 13:48:58 -0300

    feat: adicionando components

---
+++ src/app/components/modality/feedback/feedback.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { Contato } from 'src/app/models/contato';
+import { ModalityService } from 'src/app/services/modality.service';
+
+@Component({
+  selector: 'app-feedback',
+  templateUrl: './feedback.component.html',
+  styleUrls: ['./feedback.component.css']
+})
+export class FeedbackComponent implements OnInit {
+
+  public form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private contatoService: ModalityService,
+              private toastr: ToastrService,
+              private spinner: NgxSpinnerService) { }
+
+  ngOnInit() {
+    this.validator();
+  }
+
+  public get f(): any
+  {
+    return this.form.controls;
+  }
+
+  public validator(): void
+  {
+    this.form = this.formBuilder.group({
+      nome: ['', Validators.minLength(3)],
+      celular: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      cep: ['', Validators.required],
+      areaAtuacao: ['', Validators.required],
+      faturamento: ['', Validators.required],
+      instagram: ['', Validators.required]
+    });
+  }
+
+  public cadastrarContato(): void
+  {
+    this.spinner.show();
+    this.contatoService.post(this.form.value).subscribe(
+      (contato: Contato) => {
+        this.toastr.success('Em breve entraremos em contato!');
+        console.log("success");
+        this.form.reset();
+        document.getElementById("social-media")?.scrollIntoView({behavior:'smooth'});
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    ).add(() => this.spinner.hide());
+  }
+
+}
